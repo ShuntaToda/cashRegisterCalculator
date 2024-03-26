16e8dba9cb7051a74adc1436c964ea795ccc4a44
ShuntaToda <todashuntawork@gmail.com>
2024-03-26 11:26:59 +0900

    JapaneseChangeを追加

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,66 @@
 class Change {
   // このクラスはあくまで値を保持することを目的とする
-  price: number;
-  amount: number;
-  type: string;
+  change: number;
 
-  constructor(price: number, amount: number, type: string) {
-    this.price = price;
-    this.amount = amount;
-    this.type = type;
-  }
-
-  // 今後変更される可能性が高いからChangeクラスから出した方がいい
-  dispayChangeDetails() {
-    return `${this.price}円${this.type} ${this.amount}枚`;
+  constructor(change: number) {
+    this.change = change;
   }
 }
+
 type MoneyTypes = { [key: number]: string };
+type ChangeDetails = { [key: number]: number };
 
-const moneyTypes: MoneyTypes = {
-  // 10個くらいだったらベタが書きでいいかも
-  10000: "紙幣",
-  5000: "紙幣",
-  1000: "紙幣",
-  500: "硬貨",
-  100: "硬貨",
-  50: "硬貨",
-  10: "硬貨",
-  5: "硬貨",
-  1: "硬貨",
-};
+class JapaneseChange extends Change {
+  constructor(change: number) {
+    super(change);
+  }
 
-interface ChangeItem {
-  item: Change;
+  static moneyTypes: MoneyTypes = {
+    10000: "紙幣",
+    5000: "紙幣",
+    1000: "紙幣",
+    500: "硬貨",
+    100: "硬貨",
+    50: "硬貨",
+    10: "硬貨",
+    5: "硬貨",
+    1: "硬貨",
+  };
+
+  calcDetail(): ChangeDetails {
+    let change = paid - price;
+    // 通貨の価値が高い順にソートして配列にする
+    const denominations = Object.keys(JapaneseChange.moneyTypes)
+      .map(Number)
+      .sort((a, b) => b - a);
+    let result: ChangeDetails = {};
+
+    denominations.forEach((denomination) => {
+      const count = Math.floor(change / denomination);
+      if (count > 0) {
+        result = { ...result, [denomination]: count };
+        change -= denomination * count;
+      }
+    });
+    return result;
+  }
 }
+const displayChange = (change: JapaneseChange) => {
+  const changeDetails: ChangeDetails = change.calcDetail();
+
+  // 通貨の価値が高い順にソートして配列にする
+  const sortedChangeDetails = Object.keys(changeDetails)
+    .map(Number)
+    .sort((a, b) => b - a);
+
+  console.log(`お釣りは、${change.change}円です。`);
+  sortedChangeDetails.forEach((denomination) => {
+    console.log(`${denomination}円${JapaneseChange.moneyTypes[denomination]}が${changeDetails[denomination]}枚`);
+  });
+};
 
 const input: string = require("fs").readFileSync("/dev/stdin", "utf8");
 const [price, paid] = input.split(" ").map((item) => parseInt(item));
 
-const bussinessLogic = (price: number, paid: number) => {
-  let changeArray: Change[] = [];
-
-  // ここに計算が入る
-  let change = paid - price;
-  const denominations = Object.keys(moneyTypes)
-    .map(Number)
-    .sort((a, b) => b - a);
-
-  for (const denomination of denominations) {
-    const count = Math.floor(change / denomination);
-    if (count > 0) {
-      changeArray.push(new Change(denomination, count, moneyTypes[denomination]));
-      change -= denomination * count;
-    }
-  }
-  // お釣り配列だと複数の支払いのお釣りだと思ってしまう
-  // changeByCoinsなどが良さそう
-  return changeArray;
-};
-
-const dispayChange = (changeResultArray: Change[], change: number) => {
-  console.log(`お釣りは、${change}円です。`);
-  changeResultArray.forEach((item) => {
-    console.log(item.dispayChangeDetails());
-  });
-};
-
-const changeResultArray = bussinessLogic(price, paid);
-const changeResult = paid - price;
-dispayChange(changeResultArray, changeResult);
+const change: JapaneseChange = new JapaneseChange(paid - price);
+displayChange(change);
